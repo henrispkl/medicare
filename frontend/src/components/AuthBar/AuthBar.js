f5315665c6061f6c1b1cb945d796579abd3d623f
henrispkl <henriqueirineu10@hotmail.com>
2020-07-14 18:18:09 -0300

    Create AuthBar component

--- frontend/src/components/AuthBar/AuthBar.js
+++ frontend/src/components/AuthBar/AuthBar.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import styles from './AuthBar.module.css';
+import { connect } from 'react-redux';
+import { setAuthModal } from '../../store/actions/appActions';
+import { logout } from '../../store/actions/authActions';
 
 const AuthBar = (props) => {
+  const openAuthModal = () => {
+    props.dispatch(setAuthModal(true));
+  };
+
+  const logoutHandler = () => {
+    props.dispatch(logout());
+  };
+
   return (
     <div className={styles.AuthBar}>
-      <div styles={styles.Link}>Login/Register</div>
+      <div className={styles.Link} onClick={openAuthModal}>
+        Login/Register
+      </div>
+
+      {props.isAuthenticated && (
+        <div className={styles.Link} onClick={logoutHandler}>
+          Logout
+        </div>
+      )}
     </div>
   );
 };
 
-export default AuthBar;
+export default connect((store) => ({
+  authModal: store.app.authModal,
+  isAuthenticated: store.auth.isAuthenticated,
+}))(AuthBar);
