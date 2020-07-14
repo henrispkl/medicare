bf44a9509f543d50a8c2eebc907f96713399c304
henrispkl <henriqueirineu10@hotmail.com>
2020-07-14 18:17:22 -0300

    Change click prop to onClick

--- frontend/src/components/Buttons/PrimaryButton/PrimaryButton.js
+++ frontend/src/components/Buttons/PrimaryButton/PrimaryButton.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './PrimaryButton.module.css';
 
-const PrimaryButton = props => {
+const PrimaryButton = (props) => {
   const classes = [props.className, styles.PrimaryButton, 'PrimaryButton'];
 
   if (props.inactive) {
     classes.push(styles.Inactive);
   }
-  
+
   return (
-    <div className={classes.join(' ')} onClick={props.click}>
+    <div className={classes.join(' ')} onClick={props.onClick}>
       {props.children}
     </div>
   );
