1917b1e2ef274adb9ee9add7a9d127adc9c2a701
henrispkl <henriqueirineu10@hotmail.com>
2020-07-15 14:35:26 -0300

    Change style of AuthBar

--- frontend/src/components/AuthBar/AuthBar.js
+++ frontend/src/components/AuthBar/AuthBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from './AuthBar.module.css';
 import { connect } from 'react-redux';
 import { setAuthModal } from '../../store/actions/appActions';
@@ -16,12 +16,20 @@ const AuthBar = (props) => {
   return (
     <div className={styles.AuthBar}>
       {props.isAuthenticated ? (
-        <div className={styles.Link} onClick={logoutHandler}>
-          Logout
-        </div>
+        <Fragment>
+          <div className={styles.User}>
+            <div className={styles.UserIcon}>
+              <i className="far fa-user-circle"></i>
+            </div>
+            <div className={styles.UserName}>{props.user.name}</div>
+          </div>
+          <div className={styles.Link} onClick={logoutHandler}>
+            <i className="fas fa-sign-out-alt"></i> Logout
+          </div>
+        </Fragment>
       ) : (
         <div className={styles.Link} onClick={openAuthModal}>
-          Login/Register
+          <i className="fas fa-sign-in-alt"></i> Sign-in
         </div>
       )}
     </div>
@@ -31,4 +39,5 @@ const AuthBar = (props) => {
 export default connect((store) => ({
   authModal: store.app.authModal,
   isAuthenticated: store.auth.isAuthenticated,
+  user: store.auth.user,
 }))(AuthBar);
